120d529c7fecd024defcf8a4ae007da3b2b90d4a
ElliotDAM <elliotrguez.8@gmail.com>
2023-09-28 00:20:10 +0000

    Subiendo ejercicio 3

--- Unidad-1/Tareas/Tarea3/src/Ejercicio3While.java
+++ Unidad-1/Tareas/Tarea3/src/Ejercicio3While.java
@@ -2,7 +2,7 @@
  * @author ElliotDAM
  * Ejercicio número 3
  */
-public class Ejercicio3 {
+public class Ejercicio3BucleFor {
 
     /**
      * Desarrolle un algoritmo que realice la sumatoria de los números enteros comprendidos entre el 1 y el 10, es decir, 1 + 2 + 3 + …. + 10. 
@@ -10,6 +10,12 @@ public class Ejercicio3 {
     * @param args
      */
     public static void main(String[] args) {
-        
+        System.out.println("Sumatoria entre números del 1 al 10.");
+        int suma = 0;
+        while(i <= 10){
+            suma = suma + 1;
+            i++;
+        }
+        System.out.println("La suma es: "+ suma);
     }
 }
