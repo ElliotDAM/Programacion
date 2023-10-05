84bf4f2b599aa27246f2ed645cdd33f07a27447b
ElliotDAM <elliotrguez.8@gmail.com>
2023-10-05 22:00:44 +0000

    a ver si esta se sube

--- Unidad-1/Tarea3/src/main/java/Ejercicio1.java
+++ Unidad-1/Tarea3/src/main/java/Ejercicio1.java
@@ -16,27 +16,17 @@ public class Ejercicio1 {
      * @param args
      */
     public static void main(String[] args) {
-
+        int numero = 1;
+        int filas = 4;
         System.out.println("Vamos a imprimir el siguiente patrón: ");
+        
 
-        /*
-            if (i == 1){
-                System.out.println(i);
-            }else if (i > 1 && i < 4){
-                System.out.println(i+" "+(i+1));
-            }else if (i > 3 && i < 7){
-                System.out.println((i + " "+ (i+1)+" "+(i+2)));
-            } else if (i > 6 && i < 10) {
-                System.out.println((i + " "+ (i+1)+" "+(i+2)+ " "+(i+3)));
+        for (int i = 1; i <= filas; i++){
+            for (int j = 0; j < i; j++) {
+                System.out.println(numero + " ");
+                numero++;
             }
-*/
-        for (int i = 1; i <= 1; i++){
-            //Código a mejorar, no hace nada el bucle
-            System.out.println(i);
-            System.out.println(i+1+" "+(i+2));
-            System.out.println((i+3)+" "+(i+4)+" "+(i+5));
-            System.out.println((i+6)+" "+(i+7)+" "+(i+8)+" "+(i+9));
-
+            System.out.println();
         }
 
 
