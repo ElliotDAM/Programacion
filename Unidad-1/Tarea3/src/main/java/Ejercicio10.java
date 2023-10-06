c796af8dc7c5c51c72df8029f8299a1b10ae2537
ElliotDAM <elliotrguez.8@gmail.com>
2023-10-06 17:15:01 +0100

    Subiendo cambios

--- Unidad-1/Tarea3/src/main/java/Ejercicio10.java
+++ Unidad-1/Tarea3/src/main/java/Ejercicio10.java
@@ -11,12 +11,13 @@ public class Ejercicio10 {
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //Se piden datos al usuario y se inicializan variables
         System.out.println("Suma de numeros pares");
         System.out.println("Ingresa un número");
         int numero = sc.nextInt();
         int sumador = 2;
         int suma = 0;
-        
+        // Iteramos el bucle hasta el límite que puso el usuario, sumando solo los pares
         do{
             
             if (sumador % 2 == 0){
@@ -24,9 +25,10 @@ public class Ejercicio10 {
             }
             sumador ++;
         }while(numero > sumador);
+        // Se imprime la cantidad total de la suma
         System.out.println("La suma es: "+ suma);
 
-       /**
+       /** Otra solución para comprobar el codigo
         *  for (int i = 2; i < 60; i++) {
             if (i % 2 == 0){
                 int suma = i + suma;
