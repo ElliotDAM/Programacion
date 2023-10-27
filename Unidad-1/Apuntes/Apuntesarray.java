bc99d4afee86d54949b403e5f3bd0afe5dde84cb
ElliotDAM <elliotrguez.8@gmail.com>
2023-10-27 18:39:00 +0100

    Subiendo apuntes clase

---
+++ Unidad-1/Apuntes/Apuntesarray.java
@@ -0,0 +1,32 @@
+public class Apuntesarray {
+    /**
+     * Mostrar orden inverso de un array
+     * 5 numeros (10,20,30,40,50)
+     */
+
+    public int ordenInversoArray(){
+
+        int original = {10, 20, 30, 40, 50};
+        int copia = new int [original.lenght];
+
+        for (int i = original.lenght - 1; i >= 0 ; i--) {
+            copia[i] = original[original.lenght - 1 - i];
+        }
+    }
+
+    /**
+     * Mostrar todos los valores > 40
+     * @return
+     */
+    public int mostrarMenores40(){
+
+        int original = {10, 20, 30, 40, 50};
+
+        i = 0;
+
+        while (i < original.lenght){
+                
+        }
+
+    }
+}
