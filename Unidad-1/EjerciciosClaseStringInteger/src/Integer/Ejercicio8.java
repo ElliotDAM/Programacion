9dc973805e4360a1668c2dd9441f182b7c0e5fb6
ElliotDAM <elliotrguez.8@gmail.com>
2023-12-11 23:34:46 +0000

    Subiendo archivos

--- Unidad-1/EjerciciosClaseStringInteger/src/Integer/Ejercicio8.java
+++ Unidad-1/EjerciciosClaseStringInteger/src/Integer/Ejercicio8.java
@@ -4,5 +4,21 @@
  * @author ElliotDAM
  */
 public class Ejercicio8 {
+    public static void main(String[] args) {
+        int numero = 50;
+        int factorial = calcularFactorial(numero);
+
+        System.out.println("El factorial del numero es: "+factorial);
+    }
+
+    public static int calcularFactorial(int numero){
+
+        int resultado = 0;
+
+        for (int i = 1; i <= numero; i++) {
+            resultado = resultado * i;
+        }
+        return resultado;
+    }
     
 }
