3045d5148519681512bf2354d36326b898c32c63
ElliotDAM <elliotrguez.8@gmail.com>
2024-01-12 18:37:05 +0000

    Subiendo ejercicios herencia Exception

--- EjerciciosHerencia(3)/src/main/java/ies/puerto/Excepciones/ValidacionException.java
+++ EjerciciosHerencia(3)/src/main/java/ies/puerto/Excepciones/ValidacionException.java
@@ -1,5 +1,16 @@
 package ies.puerto.Excepciones;
 
-public class ValidacionException {
+import Ejercicio1.Persona;
+
+public class ValidacionException extends Exception {
+    public static void main(String[] args) {
+
+      try{
+        Persona persona = new Persona("Pepe", 22, "6879058D");
+    }catch(Exception exception){
+        System.out.println("Ha surgido una excepcion. No has escrito en el formato adecuado.");
+    }
+
+    }
     
 }
